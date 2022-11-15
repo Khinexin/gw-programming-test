a1b17272cdbfc7242d74de73a229ec8d22078182
Khine Zin <34533081+Khinexin@users.noreply.github.com>
2022-11-15 13:26:09 +0630

    first cmt

---
+++ src/main/java/com/test/gw/gwprogrammingtest/GwProgrammingTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.gw.gwprogrammingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwProgrammingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwProgrammingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/gw/gwprogrammingtest/GwProgrammingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gw.gwprogrammingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwProgrammingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
